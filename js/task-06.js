66d2f0a3732d9d9419740cfac7468bbb78a41ee1
Dima_ <chernyaev603@gmail.com>
2023-03-25 17:32:48 +0200

    change lesson 6 and 10

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,19 @@
 const inputElement = document.querySelector('#validation-input');
-const maxLength = inputElement.dataset.length;
+const correctLength = Number(inputElement.dataset.length);
+console.log(correctLength);
 
 inputElement.addEventListener('blur', correctWorld)
 
 function correctWorld(event) {
     const inputText = event.currentTarget.value;
-    
-    if(inputText.length > maxLength) {
+
+    if(inputText.length !== correctLength) {
+        inputElement.classList.remove('valid')
         inputElement.classList.add('invalid')
     } 
     
-    if (inputText.length <= maxLength) {
-        if(inputElement.classList.contains('invalid')){
-            inputElement.classList.remove('invalid')
-        }
+    if (inputText.length === correctLength) {
+        inputElement.classList.remove('invalid')
         inputElement.classList.add('valid')
-    }
+}
 }
--- js/task-10.js
+++ js/task-10.js
@@ -7,15 +7,30 @@ function getRandomHexColor() {
 const inputElement = document.querySelector('input')
 const createButton = document.querySelector('[data-create]')
 const destroyButton = document.querySelector('[data-destroy]')
+const boxesElement = document.querySelector('#boxes')
+console.log(boxesElement);
 
 
 
 createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', deleteBoxes)
 
 function createBoxes(amount) {
 amount = inputElement.value;
+let boxSize = 30;
 
-const div = `<div></div>`;
+for(let i = 0;  i < amount; i += 1) {
+  const box = document.createElement("div");
+  box.style.width = `${boxSize}px`;
+  box.style.height = `${boxSize}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  boxSize += 10;
+  boxesElement.append(box);
+}
+
 
 }
 
+function deleteBoxes() {
+  boxesElement.innerHTML = '';
+}
